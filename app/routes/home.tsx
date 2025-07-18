67b4995200677dd61a39a46f1e4c1de25a6aeea4
Shereni <info@aewebsites.co.zw>
2025-07-18 20:09:57 +0200

    Feature: Resume dummy data setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
+import NavBar from '@/app/components/NavBar';
 import type { Route } from './+types/home';
+import { resumes } from '@/contstants';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,12 +12,18 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <NavBar />
       <section className='main-section'>
         <div className='page-heading'>
           <h1>Track Your Applications And Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
       </section>
+      {resumes.map((resume) => (
+        <div>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+      ))}
     </main>
   );
 }
